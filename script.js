c209fbe20e9b81507251360440e0194df45380db
israel costa <israel_dopara@hotmail.com>
2021-09-30 19:20:53 -0300

    ponto seguro iniciando encerrando e carregando pronto

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 let iniciando =document.querySelector('#iniciando');
-let menu =document.querySelector('.tela ol');
-let carregando=document.querySelector('.carregando');
-let candidatos =document.querySelector('candidatos');
+let menu =document.querySelector('#menu');
+let carregando=document.querySelector('#carregando');
+let candidatos =document.querySelector('#candidatos');
+let encerrando =document.querySelector('#encerrando');
 
-function ligar(){
-    iniciando.style.display='block';
-    menu.style.display='block';
+function limpar(){
+iniciando.style.display='none';
+menu.style.display='none';
+carregando.style.display='none';
+candidatos.style.display='none';
+encerrando.style.display='none';
 
 }
 
+function ligar(){
+    limpar();
+    iniciando.style.display='block';
+    let pausa = setTimeout(function() {
+    limpar();
+    menu.style.display='block';
+    
+      }, 2000)
+}
+function Fcandidatos(){
+    limpar();
+    menu.style.display='none';
+    carregando.style.display='block';
+    let pausa = setTimeout(function() {
+    limpar();
+    candidatos.style.display='block';
 
+}, 2000)
+}
+function voltar(){
+    limpar();
+    carregando.style.display='block';
+    let pausa = setTimeout(function() {
+    limpar();
+    menu.style.display='block';
+    
+      }, 1500)
+}
+function encerrar(){
+    limpar();
+    encerrando.style.display='block';
+    let pausa = setTimeout(function() {
+    limpar();
+
+      }, 3000)
+}
