19a9bdd30684a3a1f39001794b3d33a81151a1d8
israel costa <israel_dopara@hotmail.com>
2021-09-30 18:00:43 -0300

    implementação do menu

---
+++ script.js
@@ -0,0 +1,11 @@
+let iniciando =document.querySelector('#iniciando');
+let menu =document.querySelector('.tela ol');
+let carregando=document.querySelector('.carregando');
+let candidatos =document.querySelector('candidatos');
+
+function ligar(){
+    iniciando.style.display='block';
+    menu.style.display='block';
+
+}
+
