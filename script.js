5df27de1f143f0546d69f5c66a33441a9642ae62
israel costa <israel_dopara@hotmail.com>
2021-10-01 19:09:54 -0300

    começo das propostas

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let candidatos =document.querySelector('#candidatos');
 let encerrando =document.querySelector('#encerrando');
 let tela =document.querySelector('.tela');
 let informações =document.querySelector('#informações');
+let propostas =document.querySelector('#propostas')
 let ligado=false;
 let estanomenu=false;
 
@@ -15,6 +16,32 @@ carregando.style.display='none';
 candidatos.style.display='none';
 encerrando.style.display='none';
 informações.style.display='none';
+propostas.style.display='none';
+}
+
+function clicou(n){
+    switch(n){
+        case 9 :
+         ligar();
+        break;
+        case 1 :
+            Fcandidatos();
+            break;
+        case 0 :
+            voltar();
+            break;
+        case 2 :
+            Finformações();
+            break;
+        case 3 :
+            Fpropostas();
+            break;
+        case 5 :
+            encerrar();
+            break;
+        default:
+            console.log("errou !");
+    }
 }
 
 function ligar(){
@@ -35,7 +62,6 @@ function Fcandidatos(){
         if(estanomenu==true){
             limpar();
             estanomenu=false;
-            menu.style.display='none';
             carregando.style.display='block';
             let pausa = setTimeout(function() {
             limpar();
@@ -83,6 +109,20 @@ function Finformações(){
             let pausa = setTimeout(function() {
             limpar();
             informações.style.display='block';
+
+        }, 1000)}
+    }
+
+}
+function Fpropostas(){
+    if(ligado==true){
+        if(estanomenu==true){
+            limpar();
+            estanomenu=false;
+            carregando.style.display='block';
+            let pausa = setTimeout(function() {
+            limpar();
+            propostas.style.display='block';
 
         }, 1000)}
     }
